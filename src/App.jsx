bb3c1895462ca70eb58ceac095abf1f7cc0f4252
ATM Abdullah Al Mamun <atmabdullaham@gmail.com>
2025-11-10 07:32:57 +0600

    create vite project with react uning tailwind and daisyui

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-2xl font-bold">
+          Just a vite project with tailwind and daisyui
+        </h1>
+        <button className="btn">click me</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
