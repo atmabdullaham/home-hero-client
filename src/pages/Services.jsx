a96c1d3fae94bd683a61699677fd38b2464c3c0a
ATM Abdullah Al Mamun <atmabdullaham@gmail.com>
2025-11-13 11:59:52 +0600

    added search functionality and filter by category

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { FaSearch } from "react-icons/fa";
 import ServiceCard from "../components/ServiceCard";
 import useAxiosPublic from "../hooks/useAxiosPublic";
 
@@ -6,9 +7,16 @@ const Services = () => {
   const axiosPublic = useAxiosPublic();
   const [allServices, setAllServices] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [category, setCategory] = useState("all");
+  const [search, setSearch] = useState("");
+
   useEffect(() => {
+    const params = {};
+
+    if (category && category !== "all") params.category = category;
+    if (search) params.search = search;
     axiosPublic
-      .get("/services")
+      .get("/services", { params })
       .then((data) => {
         setAllServices(data.data);
       })
@@ -17,7 +25,7 @@ const Services = () => {
           setLoading(false);
         }, 100);
       });
-  }, [axiosPublic]);
+  }, [axiosPublic, category, search]);
 
   if (loading)
     return (
@@ -27,12 +35,46 @@ const Services = () => {
     );
 
   return (
-    <div className="w-11/12 mx-auto">
-      This is services page {allServices.length}
-      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5">
-        {allServices.map((service) => (
-          <ServiceCard key={service._id} service={service}></ServiceCard>
-        ))}
+    <div className="w-11/12 mx-auto py-10">
+      {/* search input */}
+      <div className="flex items-center justify-center">
+        <label className="input items-center gap-2 border focus-within:outline-0 focus-within:border-cyan-600 rounded-lg">
+          <FaSearch className="text-gray-500" />
+          <input
+            type="search"
+            placeholder="Search service..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            className=""
+          />
+        </label>
+      </div>
+      <h2 className="text-center text-3xl font-bold py-4">
+        Available Services: ({allServices.length})
+      </h2>
+      <div className="grid grid-cols-1 md:grid-cols-12 md:gap-5">
+        <div className="flex flex-col items-center col-span-2">
+          <div className="w-full">
+            <select
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+              className="select select-bordered w-full"
+            >
+              <option value="all">All Categories</option>
+              <option value="cleaning">Cleaning</option>
+              <option value="plumbing">Plumbing</option>
+              <option value="repair">Repair</option>
+              <option value="electrical">Electrical</option>
+            </select>
+          </div>
+          <div>Search by Price Range</div>
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 col-span-10">
+          {allServices.map((service) => (
+            <ServiceCard key={service._id} service={service}></ServiceCard>
+          ))}
+        </div>
       </div>
     </div>
   );
